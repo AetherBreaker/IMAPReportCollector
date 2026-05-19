53789dff816c969ba898c51b41ccb8434c4ba27a
Jacob Ogden <jacob.ogden@yahoo.com>
2026-05-19 14:50:55 -0400

    Reworked to use ftp_adapter from other project

--- src/email_processing.py
+++ src/email_processing.py
@@ -12,53 +12,20 @@ else:
 
 from asyncio import TaskGroup, to_thread
 from asyncio.queues import Queue
-from ftplib import FTP, _SSLSocket  # type: ignore
 from io import BytesIO
-from json import loads
 from logging import getLogger
 from pathlib import PurePosixPath
 from re import Pattern, compile
-from socket import gaierror
-from typing import NoReturn, Self
+from typing import NoReturn
 
-from environment_init_vars import SETTINGS
 from err_handling import handle_fatal_exc_async
+from ftp_adapter import AdaptedSFTP, FTPAdapter, ServerNotAvailableError, SFTSFTPClient
 from imap_tools import MailMessage
 
 logger = getLogger(__name__)
 
 
-class ServerNotAvailableError(ConnectionError):
-  pass
-
-
-class SFTFTPClient(FTP):
-  creds = loads(SETTINGS.sft_ftp_creds_file.read_text())
-  BASE_DIR = PurePosixPath("/FTX Scheduled Reports")
-
-  def __enter__(self) -> Self:
-    try:
-      self.connect(host=self.creds["HOST"], port=self.creds["PORT"])
-      self.login(user=self.creds["USER"], passwd=self.creds["PWD"])
-    except ConnectionRefusedError as e:
-      raise ServerNotAvailableError(
-        f"Could not connect to FTP server at {self.creds['HOST']}:{self.creds['PORT']}"
-        f"\n Server exists but is not running an FTP service or is blocking the connection."
-      ) from e
-    except TimeoutError as e:
-      raise ServerNotAvailableError(
-        f"Connection to FTP server at {self.creds['HOST']}:{self.creds['PORT']} timed out."
-        f"\n Server may be offline or experiencing connectivity issues."
-      ) from e
-    except gaierror as e:
-      raise ServerNotAvailableError(f"FTP server hostname {self.creds['HOST']} could not be resolved.\n DNS has likely failed") from e
-    return self
-
-  def __exit__(self, exc_type, exc_val, exc_tb) -> None:
-    self.quit()
-
-
-# @handle_fatal_exc_async
+@handle_fatal_exc_async
 async def direct_email_processing(queue: Queue[MailMessage]) -> NoReturn:
   """Continuously check for new emails and process them."""
   async with TaskGroup() as subtasks:
@@ -78,6 +45,9 @@ SUBJECT_PATTERN: Pattern = compile(
   r"\d{1,2}, \d{4} \d{1,2}:\d{2} (AM|PM))$"
 )
 
+SWEETFIRE_SFTP: FTPAdapter[AdaptedSFTP] = FTPAdapter(SFTSFTPClient, container_cls="")
+BASE_DIR = PurePosixPath("/upload")
+
 
 def process_email(email_data: MailMessage, queue: Queue[MailMessage]) -> None:
   # sourcery skip: extract-method
@@ -90,17 +60,23 @@ def process_email(email_data: MailMessage, queue: Queue[MailMessage]) -> None:
     # timestamp = match.group("timestamp")
     logger.info(f"Email subject matched expected pattern. Extracted report name: '{report_name}'")
 
+    logger.info("Testing FTP connection to server...")
+    if not SWEETFIRE_SFTP.test_connection():
+      logger.error("FTP server is not available. Re-queuing email for later processing.")
+      queue.put_nowait(email_data)
+      return
+
     try:
-      with SFTFTPClient() as ftp_client:
+      with SWEETFIRE_SFTP.start_session() as sftp_client:
         logger.info(f"Connected to FTP server. Preparing to upload attachments for report '{report_name}'")
-        target_folder = ftp_client.BASE_DIR / report_name
+        target_folder = BASE_DIR / report_name
 
         # check if the a directory with a name that matches the report name exists on the FTP server, if not create it
         logger.info(f"Querying FTP for {target_folder}")
-        dirs = [entry for entry in ftp_client.mlsd(path=ftp_client.BASE_DIR.as_posix())]
+        dirs = [entry for entry in sftp_client.listdir(path=BASE_DIR.as_posix())]
         logger.info(f"Query for {target_folder} complete")
         if str(target_folder) not in dirs:
-          ftp_client.mkd(target_folder.as_posix())
+          sftp_client.makedir(target_folder.as_posix())
           logger.info(f"Created new directory on FTP server: {target_folder}")
 
         logger.info("Directory check complete. Starting attachment upload...")
@@ -109,14 +85,12 @@ def process_email(email_data: MailMessage, queue: Queue[MailMessage]) -> None:
 
         for remote_path, payload in remote_paths.items():
           bio = BytesIO(payload)
-          with ftp_client.transfercmd(f"STOR {remote_path.as_posix()}") as conn:
-            logger.info(f"Transfer initiated for attachment '{remote_path.name}' to '{remote_path.as_posix()}'")
-            while buffer := bio.read(8192):
-              conn.sendall(buffer)
-            if _SSLSocket is not None and isinstance(conn, _SSLSocket):
-              conn.unwrap()  # type: ignore
-          logger.info(f"Attachment '{remote_path.name}' uploaded successfully to '{remote_path.as_posix()}'")
-          ftp_client.voidresp()
+          sftp_client.upload_file(
+            remote_path=remote_path.as_posix(),
+            callback=bio.read,
+            file_size=len(payload),
+          )
+          logger.info(f"Attachment '{remote_path.name}' uploaded to '{remote_path.as_posix()}'")
 
         logger.info(f"Successfully processed email '{email_data.subject}' and uploaded attachments to FTP server.")
 
