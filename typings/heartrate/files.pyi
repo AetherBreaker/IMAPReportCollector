bebef9d619d59faaad55285bbc3664c272a8f727
Jacob Ogden <jacob.ogden@yahoo.com>
2026-07-23 11:16:33 -0400

    refactor: finished heartrate type stubs

--- typings/heartrate/files.pyi
+++ typings/heartrate/files.pyi
@@ -1,9 +1,9 @@
-# ruff: noqa: ANN001
-# pyright: reportMissingParameterType=false
+# Standard library imports
+from collections.abc import Callable
+from os import PathLike
+from re import Pattern
+from typing import Any, Literal
 
-def all(_path):  # -> Literal[True]:  # noqa: A001
-  ...
-def path_contains(*subs):  # -> Callable[..., bool]:
-  ...
-def contains_regex(pattern):  # -> Callable[..., bool]:
-  ...
+def all(_path: Any) -> Literal[True]: ...  # noqa: A001
+def path_contains(*subs: str) -> Callable[[str], bool]: ...
+def contains_regex(pattern: str | Pattern[str]) -> Callable[[PathLike | str], bool]: ...
